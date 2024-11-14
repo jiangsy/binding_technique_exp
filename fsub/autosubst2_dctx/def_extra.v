6c41fd497e31f1cb5c6d9df2798b86ab2c7574a3
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-11-14 12:40:57 +0800

    adding fsub/autosubst2_dctx subtyping and typing rules

---
+++ fsub/autosubst2_dctx/def_extra.v
@@ -0,0 +1,76 @@
+Require Import List.
+
+Require Import common.prop_as_core.
+Require Import common.prop_as_unscoped.
+Require Import fsub.autosubst2_dctx.def_as2.
+
+Import SubstNotations.
+Import UnscopedNotations.
+
+Definition ctx := list typ.
+
+Inductive lookup {T} : nat -> list T -> T -> Prop :=
+| here A Γ : lookup 0 (A :: Γ) A
+| there n Γ A B : lookup n Γ A -> lookup (S n) (cons B Γ) A.
+
+Fixpoint wf_typ Δ A := match A with
+  | typ_var X => exists B, lookup X Δ B
+  | typ_top => True
+  | typ_arr A B => wf_typ Δ A /\ wf_typ Δ B
+  | typ_all A B => wf_typ Δ A /\ wf_typ (A :: Δ) B
+end.
+
+Notation "Δ ⊢ A" := (wf_typ Δ A) (at level 99).
+
+Reserved Notation "Δ ⊢ A <: B" 
+  (at level 99, A at next level, no associativity).
+Inductive subtyping : ctx -> typ -> typ -> Prop :=
+| sub_top Δ A :
+  Δ ⊢ A ->
+  Δ ⊢ A <: typ_top
+| sub_tvar_refl Δ X :
+  Δ ⊢ typ_var X ->
+  Δ ⊢ typ_var X <: typ_var X
+| sub_tvar_bound Δ X A B: 
+  lookup X Δ A ->
+  Δ ⊢ A <: B ->
+  Δ ⊢ typ_var X <: B
+| sub_arrow Δ A1 A2 B1 B2 : 
+  Δ ⊢ B1 <: A1 ->
+  Δ ⊢ A2 <: B2 ->
+  Δ ⊢ (typ_arr A1 A2) <: (typ_arr B1 B2)
+| sub_all Δ A1 A2 B1 B2 : 
+  Δ ⊢ B1 ->
+  Δ ⊢ B1 <: A1 ->
+  (B1 :: Δ) ⊢ A2 <: B2 ->
+  Δ ⊢ (typ_all A1 A2) <: (typ_all B1 B2)
+where "Δ ⊢ A <: B" := (subtyping Δ A B).
+
+Reserved Notation "Δ ; Γ ⊢ t : A" 
+  (at level 99, Γ at next level, t at next level, no associativity).
+Inductive typing : ctx -> ctx -> exp -> typ -> Prop :=
+| typing_var Δ Γ i A :
+  lookup i Γ A ->
+  Δ ; Γ ⊢ (exp_var i) : A
+| typing_abs Δ Γ t A B :
+  Δ ⊢ A ->
+  Δ ; (A :: Γ) ⊢ t : B ->
+  Δ ; Γ ⊢ (exp_abs A t) : (typ_arr A B)
+| typing_app Δ Γ s t A B :
+  Δ ; Γ ⊢ t : (typ_arr A B) ->
+  Δ ; Γ ⊢ s : A ->
+  Δ ; Γ ⊢ (exp_app t s) : B
+| typing_tabs : forall Δ Γ t A B,
+  Δ ⊢ A ->
+  (A :: Δ) ; (map (ren_typ ↑) Γ) ⊢ t : B ->
+  Δ ; Γ ⊢ (exp_tabs A t) : (typ_all A B)
+| typing_tapp : forall Δ Γ t A B A' B',
+  Δ ; Γ ⊢ t : (typ_all A B) ->
+  Δ ⊢ A' <: A ->
+  B' = B [A'..] ->
+  Δ ; Γ ⊢ exp_tapp t A' : B'
+| typing_sub : forall Δ Γ t A B,
+  Δ ; Γ ⊢ t : A ->
+  Δ ⊢ A <: B ->
+  Δ ; Γ ⊢ t : B
+where "Δ ; Γ ⊢ t : A" := (typing Δ Γ t A).
--- fsub/autosubst2_dctx/language.sig
+++ fsub/autosubst2_dctx/language.sig
@@ -5,9 +5,10 @@ exp : Type
 -- the constructors for typ
 typ_arr : typ -> typ -> typ
 typ_all : typ -> (bind typ in typ) -> typ
+typ_top : typ
 
 -- the constructors for exp
 exp_app  : exp -> exp -> exp
 exp_abs  : typ -> (bind exp in exp) -> exp
 exp_tapp : exp -> typ -> exp
-exp_tabs : (bind typ in exp) -> exp
+exp_tabs : typ -> (bind typ in exp) -> exp
