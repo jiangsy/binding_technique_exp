4edb91b15684ec3da09ac30eb615cb5a38746945
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-11-17 00:55:49 +0800

    update the entry file

--- README.v
+++ README.v
@@ -1,3 +1,5 @@
+Require Import common.prop_as_core.
+Require Import common.prop_as_unscoped.
 Require stlc.autosubst2.prop_type_safety.
 Require stlc.lngen.prop_type_safety.
 Require systemf.autosubst2.prop_type_safety.
@@ -7,9 +9,12 @@ Require fsub.autosubst2_dctx.prop_typing.
 Section stlc.
 
   Section autosubst2.
+    Import stlc.autosubst2.def_as2.
     Import stlc.autosubst2.def_extra.
     Import stlc.autosubst2.prop_type_safety.
 
+    Print typing.
+    Print step.
     Check preservation.
     Print Assumptions preservation. 
     Check progress.
@@ -17,9 +22,13 @@ Section stlc.
   End autosubst2.
 
   Section lngen.
+    Import Metalib.Metatheory.
+    Import stlc.lngen.def_ott.
     Import stlc.lngen.def_extra.
     Import stlc.lngen.prop_type_safety.
     
+    Print typing.
+    Print step.
     Check preservation.
     Print Assumptions preservation. 
     Check progress.
@@ -32,8 +41,12 @@ End stlc.
 Section systemf.
 
   Section autosubst2.
+    Import systemf.autosubst2.def_as2.
     Import systemf.autosubst2.def_extra.
     Import systemf.autosubst2.prop_type_safety.
+
+    Print typing.
+    Print step.
 
     Check preservation.
     Print Assumptions preservation. 
@@ -42,8 +55,13 @@ Section systemf.
   End autosubst2.
 
   Section lngen.
+    Import Metalib.Metatheory.
+    Import systemf.lngen.def_ott.
     Import systemf.lngen.def_extra.
     Import systemf.lngen.prop_type_safety.
+
+    Print typing.
+    Print step.
     
     Check preservation.
     Print Assumptions preservation. 
@@ -57,8 +75,12 @@ End systemf.
 Section fsub.
 
   Section autosubst2.
+    Import fsub.autosubst2_dctx.def_as2.
     Import fsub.autosubst2_dctx.def_extra.
     Import fsub.autosubst2_dctx.prop_typing.
+
+    Print typing.
+    Print step.
 
     Check preservation.
     Print Assumptions preservation. 
