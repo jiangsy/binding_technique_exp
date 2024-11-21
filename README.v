9f94436e74d0c0f330dbe138a6c1adca7d60e17d
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-11-21 11:02:50 +0800

    print more info in the entry file

--- README.v
+++ README.v
@@ -10,8 +10,9 @@ Require fsub.autosubst2_sctx.prop_typing.
 
 
 Section stlc.
-
+  Goal True. idtac "". idtac "". idtac "Simple Typed Lambda Calculus". idtac "". idtac "". Abort.
   Section autosubst2.
+    Goal True. idtac "". idtac "Autosusbt2". idtac "". Abort.
     Import stlc.autosubst2.def_as2.
     Import stlc.autosubst2.def_extra.
     Import stlc.autosubst2.prop_type_safety.
@@ -25,6 +26,7 @@ Section stlc.
   End autosubst2.
 
   Section lngen.
+    Goal True. idtac "". idtac "LNgen". idtac "". Abort.
     Import Metalib.Metatheory.
     Import stlc.lngen.def_ott.
     Import stlc.lngen.def_extra.
@@ -42,8 +44,9 @@ End stlc.
 
 
 Section systemf.
-
+  Goal True. idtac "". idtac "". idtac "System F". idtac "". idtac "". Abort.
   Section autosubst2.
+    Goal True. idtac "". idtac "Autosusbt2". idtac "". Abort.
     Import systemf.autosubst2.def_as2.
     Import systemf.autosubst2.def_extra.
     Import systemf.autosubst2.prop_type_safety.
@@ -58,6 +61,7 @@ Section systemf.
   End autosubst2.
 
   Section lngen.
+    Goal True. idtac "". idtac "LNgen". idtac "". Abort.
     Import Metalib.Metatheory.
     Import systemf.lngen.def_ott.
     Import systemf.lngen.def_extra.
@@ -76,8 +80,11 @@ End systemf.
 
 
 Section fsub.
+  Goal True. idtac "". idtac "". idtac "System F-sub". idtac "". idtac "". Abort.
 
   Section autosubst2_dctx.
+    Goal True. idtac "". idtac "Autosusbt2". idtac "". Abort.
+
     Import fsub.autosubst2_dctx.def_as2.
     Import fsub.autosubst2_dctx.def_extra.
     Import fsub.autosubst2_dctx.prop_typing.
@@ -94,6 +101,8 @@ Section fsub.
   (* for reference, this is another version using combined context, with plain De Brujin *)
   (* https://www.seas.upenn.edu/~plclub/poplmark/vouillon.html *)
   Section autosubst2_sctx.
+    Goal True. idtac "". idtac "Autosusbt2". idtac "". Abort.
+
     Import fsub.autosubst2_sctx.def_as2.
     Import fsub.autosubst2_sctx.def_extra.
     Import fsub.autosubst2_sctx.prop_typing.
